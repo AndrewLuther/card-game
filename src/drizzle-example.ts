959a62f7f4dc95b4bb0e119b39d4b73281e57841
Andrew Luther <andrewgluther@gmail.com>
2026-04-29 12:25:29 -0400

    project initial structure

---
+++ src/drizzle-example.ts
@@ -0,0 +1,32 @@
+import { db } from "./db/db";
+import { userTable } from "./db/schema";
+import { eq } from "drizzle-orm";
+
+async function main() {
+  const userResult = await db
+    .insert(userTable)
+    .values({
+      name: "Andrew",
+      discordSnowflake: "194910567188791297",
+    })
+    .returning();
+
+  const user = userResult[0]!;
+
+  console.log("New user created!", user);
+  const users = await db.select().from(userTable);
+  console.log("Getting all users from the database: ", users);
+
+  await db
+    .update(userTable)
+    .set({
+      name: "thedollylama",
+    })
+    .where(eq(userTable.id, user.id));
+
+  console.log("User info updated!");
+  //await db.delete(userTable).where(eq(userTable.id, user.id));
+  //console.log("User deleted!");
+}
+
+main();
---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import "dotenv/config"; // allows us to read .env file
+
+import {
+  ChatInputCommandInteraction,
+  Client,
+  Events,
+  GatewayIntentBits,
+  REST,
+  Routes,
+  SlashCommandBuilder,
+} from "discord.js";
+import { db } from "./db/db";
+import { userTable } from "./db/schema";
+
+const TOKEN = process.env.DISCORD_BOT_TOKEN!;
+const CLIENT_ID = process.env.DISCORD_CLIENT_ID!;
+const GUILD_ID = process.env.DISCORD_GUILD_ID; // optional
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+type Command = {
+  data: SlashCommandBuilder;
+  execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
+};
+
+const commands: Command[] = [
+  {
+    data: new SlashCommandBuilder()
+      .setName("ping")
+      .setDescription("Replies with Pong!"),
+    async execute(interaction) {
+      const count = await db.$count(userTable);
+      await interaction.reply(`Pong!, we have ${count} users in the db.`);
+    },
+  },
+];
+
+// Register
+const rest = new REST({ version: "10" }).setToken(TOKEN);
+
+async function register() {
+  const body = commands.map((c) => c.data.toJSON());
+
+  if (GUILD_ID) {
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body,
+    });
+  } else {
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body });
+  }
+
+  console.log("Registered slash commands");
+}
+
+// Handle slash commands
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = commands.find((c) => c.data.name === interaction.commandName);
+  if (!command) return;
+
+  await command.execute(interaction);
+});
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+(async () => {
+  await register();
+  await client.login(TOKEN);
+})();
